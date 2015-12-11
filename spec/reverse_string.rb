7200afc9025e1734697a43dc37c9c65f5672328a
Sumeet Jain <sumeet@sumeetjain.com>
2015-12-11 15:56:15 -0600

    Update reverse_string.rb

--- spec/reverse_string.rb
+++ spec/reverse_string.rb
@@ -9,5 +9,6 @@ class ReverseStringTest < Minitest::Test
   
   def test_reverse_words
     sample_string = "Today is a good day"
+    assert_equal("day good a is Today", reverse_words(sample_string))
   end
 end
