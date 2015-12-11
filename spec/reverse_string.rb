ae043d1d5f2f92a4cbe4e852534ff993dece94c5
Sumeet Jain <sumeet@sumeetjain.com>
2015-12-11 15:55:54 -0600

    Update reverse_string.rb

--- spec/reverse_string.rb
+++ spec/reverse_string.rb
@@ -1,1 +1,13 @@
+require 'test_helper'
+require 'reverse_string'
 
+class ReverseStringTest < Minitest::Test
+  def test_reverse_characters
+    sample_string = "Today is a good day"
+    assert_equal("yad doog a si yadoT", reverse_characters(sample_string))
+  end
+  
+  def test_reverse_words
+    sample_string = "Today is a good day"
+  end
+end
