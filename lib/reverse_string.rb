4b734deaccd14406b5bfde1958b1875c606581cb
Ryan Newell <ryanalbertnewell@gmail.com>
2016-01-08 21:36:38 +0000

    Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,13 @@
 def reverse_characters(str)
+  rev_str = []
+  new_arr = str.split("")
+  rev_str << new_arr.pop until new_arr.empty?
+  return rev_str.join("")
 end
 
 def reverse_words(str)
+    rev_str = []
+    new_arr = str.split ()
+    rev_str << new_arr.pop until new_arr.empty?
+    return rev_str.join(" ")
 end
